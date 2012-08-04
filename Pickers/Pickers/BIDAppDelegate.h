76669f2b6a946edbdbaee2f3e7d36ce5d9b5f489
Steven Senatori <senatori@mail.sfsu.edu>
2012-08-03 19:01:07 -0700

    a nother little project

---
+++ Pickers/Pickers/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Pickers
+//
+//  Created by Stephen Senatori on 8/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pickers/Pickers/BIDDatePickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDDatePickerViewController.h
+//  Pickers
+//
+//  Created by Stephen Senatori on 8/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDDatePickerViewController : UIViewController
+
+@end
